b6d697ba614675560ca223e4e88c16dd19e62d76
ReyFey <vic.betsch@gmail.com>
2022-05-15 18:18:41 +0200

    :bug: Fix error message

--- assets/vendor/php-email-form/validate.js
+++ assets/vendor/php-email-form/validate.js
@@ -18,7 +18,7 @@
       let recaptcha = thisForm.getAttribute('data-recaptcha-site-key');
       
       if( ! action ) {
-        displayError(thisForm, 'The form action property is not set!')
+        displayError(thisForm, 'Le formulaire ne fonctionne pas encore')
         return;
       }
       thisForm.querySelector('.loading').classList.add('d-block');
